6c053720cadde82182af1a50e523294137560da5
garet-rich <garet.rich@gmail.com>
2021-03-24 14:52:03 -0600

    Created gameplay file, added new gameboard methods

--- GameBoard.rb
+++ GameBoard.rb
@@ -1,4 +1,4 @@
-class GameBoard
+class Gameboard
     @@session_counter = 0
 
     def initialize(player_Name1, player_Name2)
@@ -9,16 +9,17 @@ class GameBoard
         #create ID for current game based on player names and the total number of games played
         setGameID(@player_Name1, @player_Name2)
 
-        createGameBoard()
-        displayGameBoard()
+        createBoard()
+        displayBoard()
+        updateBoard(1,1)
+        displayBoard()
     end
 
     def setGameID(player_Name1, player_Name2)
         @gameID = player_Name1 + player_Name2 + @@session_counter.to_s
     end
 
-    def createGameBoard()
-        
+    def createBoard()
         #creates a standard grid with 7 columns and 6 rows
         @board = [
             [".", ".", ".", ".", ".", ".", "."],
@@ -30,16 +31,22 @@ class GameBoard
         ]
     end
 
-    def displayGameBoard()
+    def displayBoard()
         #displays column numbers for gameplay
         puts [1, 2, 3, 4, 5, 6, 7].join(" ")
 
         #iterates through all items, adds lines together with a space
+        @board.each do |row|
+            puts row.each { |column| column}.join(" ")
+        end
+    end
+
+    def updateBoard(move, player)
         @board.each do |columns|
-            puts columns.each { |row| row}.join(" ")
+            columns.each do |row|
+            end
         end
     end
 end
 
-
-tester = GameBoard.new("Timmy", "Jimmy")
+tester = Gameboard.new("Timmy", "Jimmy")
