f1ce5ff24f5e1cc503bfd2c28b5751d81e6fadbc
garet-rich <62623715+garet-rich@users.noreply.github.com>
2021-04-14 17:56:34 -0600

    Updated functions, re-named file to Game.rb

--- Game.rb
+++ Game.rb
@@ -1,8 +1,8 @@
-class Gameplay
+class Game
     def initialize()
         puts "Hello, what is your name?"
         player_name = gets.chomp
     end
 end
 
-tester = Gameplay.new()
+tester = Game.new()
